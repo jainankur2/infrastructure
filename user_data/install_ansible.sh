36afd6fac66693e46837147a6ad0def340a83026
jainankur2 <33921060+jainankur2@users.noreply.github.com>
2021-11-25 14:36:31 +0530

    Delete user_data directory

--- user_data/install_ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install ansible -y
-sudo apt upgrade ansible -y
-
-echo "Host *" >> /home/ubuntu/.ssh/config
-echo "    StrictHostKeyChecking no" >> /home/ubuntu/.ssh/config
-chmod 400 /home/ubuntu/.ssh/config
-chown ubuntu:ubuntu /home/ubuntu/.ssh/config
